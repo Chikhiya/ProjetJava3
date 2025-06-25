e591e15b2e10ad51791e1176dda2cb9958d66a72
Chikhiya <ilham.chikhiya@gmail.com>
2025-06-25 21:52:57 +0100

    version deux

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("bonjour tout le monde");
+        System.out.println("bonsoir tout le monde");
         }
 
 }
